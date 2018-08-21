3d6f633dd401d178a9a2e9eb49466180380409e5
Meghan Gutshall <meghan.gutshall@gmail.com>
2018-08-21 03:01:18 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,5 +1,5 @@
 class Owner
-  attr_accessor :owner
+  attr_accessor :owner, :pets
   attr_reader :species, :name
   @@all = []
 
@@ -30,7 +30,7 @@ class Owner
   end
 
   def pets
-    pets = {:fishes [], :dogs [], :cats []}
+    @pets = {:fishes [], :dogs [], :cats []}
   end
 
 end
