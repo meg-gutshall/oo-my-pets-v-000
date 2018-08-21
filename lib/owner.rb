0bd8ada795abea4f732559de4e68a69c699f997a
Meghan Gutshall <meghan.gutshall@gmail.com>
2018-08-21 03:00:40 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -7,7 +7,6 @@ class Owner
     @owner = owner
     @@all << self
     @species = species
-    @pets = {:fishes [], :dogs [], :cats []}
   end
 
   def self.all
@@ -30,5 +29,8 @@ class Owner
     @name = name
   end
 
+  def pets
+    @pets = {:fishes [], :dogs [], :cats []}
+  end
 
 end
