43e1566e54a492bb404cccfb4db7c0dcfc152e11
Meghan Gutshall <meghan.gutshall@gmail.com>
2018-08-21 02:57:55 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -7,6 +7,7 @@ class Owner
     @owner = owner
     @@all << self
     @species = species
+    @name = name
   end
 
   def self.all
@@ -25,8 +26,9 @@ class Owner
     return "I am a human."
   end
 
-  def name=(name)
-    @name = name
-  end
+  # def name=(name)
+  #   @name = name
+  # end
+
 
 end
